1d657e30bdf807267f296c266c2e64d316ae22f7
bishtvaasu <bishtvaasu@gmail.com>
2025-04-22 16:40:26 +0000

    init:project setup

---
+++ next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['*','https://3000-idx-poolup-1745309924242.cluster-ikxjzjhlifcwuroomfkjrx437g.cloudworkstations.dev']
+};
+
+export default nextConfig;
