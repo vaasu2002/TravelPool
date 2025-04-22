178fc41eab230717cda4cf943b03e9d814678dc2
bishtvaasu <bishtvaasu@gmail.com>
2025-04-22 17:00:59 +0000

    api: added datetime filter

--- src/app/api/taxiPools/route.ts
+++ src/app/api/taxiPools/route.ts
@@ -6,17 +6,13 @@ import moment from 'moment-timezone';
 export async function POST(request: NextRequest) {
   const newPool = await request.json();
   const { db } = await connectToDatabase();
-
   const result = await db.collection('taxiPools').insertOne(newPool);
-
   const insertedDoc = await db.collection('taxiPools').findOne({ _id: result.insertedId });
-
   return NextResponse.json(insertedDoc);
 }
 
 export async function GET() {
   const { db } = await connectToDatabase();
-
   const nowIST = moment().tz('Asia/Kolkata');
   const allPools = await db.collection('taxiPools').find().sort({ date: -1 }).toArray();
 
